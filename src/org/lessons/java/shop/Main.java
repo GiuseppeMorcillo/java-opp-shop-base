47a18b8c393f07f2bf0bd8ddd46ff0eeab5d7211
giuseppemorcillo <beppebeppe18@gmail.com>
2025-07-24 13:13:30 +0200

    feat: aggiunto un nuovo prodotto e aggiunto nuovi metodi per chiamare nome etc

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -4,15 +4,31 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        // Creo un prodotto
+        // Creo un prodotto completo
+        Prodotto p1 = new Prodotto("Mouse", "Logitech", new BigDecimal("25.99"), new BigDecimal("0.22"));
 
-        // Altro prodotto 
-        Prodotto p2 = new Prodotto("Tastiera", "Tastiera meccanica retroilluminata", new BigDecimal(79), new BigDecimal(0.22));
+        // Creo un prodotto usando il costruttore parziale
+        Prodotto p2 = new Prodotto("Tastiera", "Corsair");
+        p2.setPrezzo(new BigDecimal("79.00"));
+        p2.setIva(new BigDecimal("0.22"));
 
-        System.out.println("Codice: " + p2.codice);
-        System.out.println("Nome: " + p2.nome);
-        System.out.println("Descrizione: " + p2.descrizione);
-        System.out.println("Prezzo base: " + p2.getPrezzoBase());
+        // Stampo info p1
+        System.out.println("=== PRODOTTO 1 ===");
+        System.out.println("Codice: " + p1.getCodice());
+        System.out.println("Nome: " + p1.getNome());
+        System.out.println("Marca: " + p1.getMarca());
+        System.out.println("Prezzo base: " + p1.getPrezzo());
+        System.out.println("Prezzo con IVA: " + p1.getPrezzoConIva());
+        System.out.println("Nome esteso: " + p1.getNomeEsteso());
+
+        System.out.println();
+
+        // Stampo info p2
+        System.out.println("=== PRODOTTO 2 ===");
+        System.out.println("Codice: " + p2.getCodice());
+        System.out.println("Nome: " + p2.getNome());
+        System.out.println("Marca: " + p2.getMarca());
+        System.out.println("Prezzo base: " + p2.getPrezzo());
         System.out.println("Prezzo con IVA: " + p2.getPrezzoConIva());
         System.out.println("Nome esteso: " + p2.getNomeEsteso());
     }
